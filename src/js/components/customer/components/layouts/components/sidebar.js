2a0bad4fe547f5669ea8aea1ddad6a9085f1e3ee
m4muchu <muchu.pkd123@gmail.com>
2019-12-24 08:07:37 +0530

    scss arch implemented

--- src/js/components/customer/components/layouts/components/sidebar.js
+++ src/js/components/customer/components/layouts/components/sidebar.js
@@ -1,39 +1,51 @@
 import React from 'react';
 import { Navigation } from '@shopify/polaris';
+import moment from 'moment';
 
 
 export const SideBar = () => {
     return (
-        <Navigation location="/">
-            <Navigation.Section
-                items={[
-                    {
-                        url: '/customer/home',
-                        label: 'Home',
-                        //icon: HomeMajorMonotone,
-                    },
-                    {
-                        url: '/path/to/place',
-                        label: 'Orders',
-                        //icon: OrdersMajorTwotone,
-                    },
-                    {
-                        url: '/path/to/place',
-                        label: 'Products',
-                        // icon: ProductsMajorTwotone,
-                    },
-                ]}
-            />
-            <Navigation.Section
-                items={[
-                    {
-                        url: '/path/to/place',
-                        label: 'Online Store',
-                        //icon: OnlineStoreMajorTwotone,
-                    },
-                ]}
-                separator
-            />
-        </Navigation>
+        <div className="global-sidebar offcanvas-collapse" id="sidebarNav" >
+            <aside className="sidebar-wrapper sidebar-collapse shadow"  >
+                <Navigation location="/">
+                    <Navigation.Section
+                        items={[
+                            {
+                                url: '/customer/home',
+                                label: 'Home',
+                                //icon: HomeMajorMonotone,
+                            },
+                            {
+                                url: '/path/to/place',
+                                label: 'Orders',
+                                //icon: OrdersMajorTwotone,
+                            },
+                            {
+                                url: '/path/to/place',
+                                label: 'Products',
+                                // icon: ProductsMajorTwotone,
+                            },
+                        ]}
+                    />
+                    <Navigation.Section
+                        items={[
+                            {
+                                url: '/path/to/place',
+                                label: 'Online Store',
+                                //icon: OnlineStoreMajorTwotone,
+                            },
+                        ]}
+                        separator
+                    />
+                </Navigation>
+                <div className="sidebar__footer">
+                    <div className="sidebar__footer-text">
+                        <p>{moment().format('YYYY')} &copy; ModRN Health, Inc.<br />privacy - Terms of Service</p>
+                    </div>
+                </div>
+                <div className="sidebar__footer--color">
+                </div>
+            </aside>
+        </div>
     );
 }
