ebe43082b3bb82fd32b0fc4186d5846349dacd35
stepinfwd <stepinfwd@gmail.com>
2019-12-24 14:55:23 +0530

    home page colour change

--- src/js/components/customer/components/dashboard/dashboard.js
+++ src/js/components/customer/components/dashboard/dashboard.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import CardComponent from 'js/components/customer/components/card/card';
+import { Card } from '@shopify/polaris'
+
 
 
 export const Dashboard = (props) => {
     return (
-        <div>
-            <div className="generic-page-header">
-                <h2 className="page-head my-0">Dashboard</h2>
-            </div>
-            <div className="Home-card-align">
-                <CardComponent name="Reports Sent" />
-                <CardComponent name="Next Daily Report In" />
-                <CardComponent name="Next Weekly Report In" />
-                <CardComponent name="Page Views" />
-                <CardComponent name="Revenue Today" />
-                <CardComponent name="Revenue This Month" />
+        <div >
 
-            </div>
-            <div className="home-graph">
-                <p className="">Graph</p>
-            </div>
+
+                <div className="generic-page-header">
+                    <h2 className="page-head my-0">Dashboard</h2>
+                </div>
+                <div className="Home-card-align">
+                    <CardComponent name="Reports Sent" />
+                    <CardComponent name="Next Daily Report In" />
+                    <CardComponent name="Next Weekly Report In" />
+                    <CardComponent name="Page Views" />
+                    <CardComponent name="Revenue Today" />
+                    <CardComponent name="Revenue This Month" />
+
+                </div>
+                <div className="home-graph">
+                    <p className="">Graph</p>
+                </div>
         </div>
     )
 }
