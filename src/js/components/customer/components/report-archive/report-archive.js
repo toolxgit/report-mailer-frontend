88ce0067fdd044d12520355ce9ea77a08dff222b
stepinfwd <stepinfwd@gmail.com>
2019-12-24 22:16:30 +0530

    report setting finished

---
+++ src/js/components/customer/components/report-archive/report-archive.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {Card} from '@shopify/polaris'
+
+export const ReportArchive = (props) => {
+    return (
+        <div>
+            <h1>kasjdkansjdhasjdhjashdj</h1>
+
+            <div className="rep-archive">
+                <Card title="Online store dashboard">
+                    <Card.Section title="Reports">
+                        <p>View a summary of your online store’s performance.</p>
+                    </Card.Section>
+
+                    <Card.Section title="Summary">
+                        <p>
+                            View a summary of your online store’s performance, including sales,
+                            visitors, top products, and referrals.
+                        </p>
+                    </Card.Section>
+                </Card>
+            </div>
+            
+            <div className="report-sheet">
+                <div className="section1">
+                    <p>store online</p>
+                    <h3>Daily Report</h3>
+                    <p>Saturday,May November26,2020</p>
+                </div>
+
+                <div className="section2">
+
+
+
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
