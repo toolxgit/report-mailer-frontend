b0752e5a3f7525a378f7f9d51a32109856f28fd5
m4muchu <muchu.pkd123@gmail.com>
2019-12-24 12:54:37 +0530

    removed unwanted code

---
+++ src/js/components/customer/components/report-settings/report-settings.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const ReportSettings = () => {
+    return (
+        <div>
+            <div className="generic-page-header">
+                <h2 className="page-head my-0">DASHBOARD</h2>
+            </div>
+        </div>
+    );
+}
--- src/js/routes/customer/portal.js
+++ src/js/routes/customer/portal.js
@@ -1,9 +1,15 @@
 import { Home } from 'js/components/customer/components/home/home';
+import { ReportSettings } from 'js/components/customer/components/report-settings/report-settings';
+
 
 export const portalRoutes = [
     {
         path: "/customer/home",
         component: Home
+    },
+    {
+        path: "/customer/report-settings",
+        component: ReportSettings
     },
     {
         path: "/",
